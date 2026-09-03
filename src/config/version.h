e77c06de25ffc1f2e00dceaa086ff098ee019115
kalendor <kalendor@hotmail.com~>
2026-09-03 18:06:54 -0400

    Reduce touch debounce to 10 ms

--- src/config/version.h
+++ src/config/version.h
@@ -2,5 +2,5 @@
 
 // Single source of truth for the firmware version string.
 // Update this one line instead of editing version text in multiple files.
-#define FIRMWARE_VERSION "1.2.9"
+#define FIRMWARE_VERSION "1.2.10"
 #define FIRMWARE_NAME "Pourover Scale"
